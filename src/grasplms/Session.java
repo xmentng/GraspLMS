7dc8942be47c0deb55edeb6a2f4633826ad2337a
USER <USER@USER-PC>
2019-03-10 21:45:20 +0100

    This application is an Online Learning Management System

---
+++ src/grasplms/Session.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grasplms;
+
+/**
+ *
+ * @author USER
+ */
+public class Session {
+  int startDate;
+  int endDate;
+  Instructor intstructor;
+  Course course;
+  Client client;
+
+}
